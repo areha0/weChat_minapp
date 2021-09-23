acd89276df66c7cb7b03d1ddc42383e52b8369db
wangshuo <2420256810@qq.com>
2021-09-23 11:50:53 +0800

    long time no see

--- 02_wechatdemo/pages/index/index.js
+++ 02_wechatdemo/pages/index/index.js
@@ -1,66 +1,55 @@
 // pages/index/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
+  // 1. 小程序中的数据是在Page中的data中定义的
+  // 2. 在wxml中使用数据时依然是用mustache语法
+  // 3. 在js文件中调用时, 使用this.data.key
+  // 4. 修改数据值时, 在this.setData()中进行修改, setData内部传入的数据是一个对象
+  // 对象内部是需要修改的  键(直接写键名)以及修改值
+
+  // 一. data数据存储
   data: {
-
+    message: "你好啊, 我是恁爹"
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  // 二. 方法的声明位置和data以及生命周期是在同一级
+  handleParent() {
+    console.log("你好啊, 我是Parent");
+  },
+  handleChild() {
+    console.log("nihaoa, 我是child");
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 三. 路由跳转, 是在事件中进行跳转操作
+  // 这里使用wx.navigateTo({}), 可以返回上一级; 还有wx.redirectTo({}), 不能返回上一级, 可以返回主界面
+  toLogs() {
+    wx.navigateTo({
+      // 使用相对路径
+      // url: '../logs/logs',
+      // 使用绝对路径, 绝对路径之前加上反斜杠
+      url: "/pages/logs/logs"
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 四. 声明周期函数
+  // onLoad(): 页面加载
+  // onShow(): 页面加载后执行(此时页面中还没有请求到数据), 页面解除隐藏后执行(路由跳转时可能会被隐藏)
+  // onready(): 页面第一次渲染后执行
+  // onHide(): 页面隐藏时执行
+  // onUnload(): 页面销毁时执行
+  onLoad(options) {
+    console.log("onLoad");
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  onShow() {
+    console.log("onShow");
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  onReady() {
+    console.log("onReady");
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  onHide() {
+    console.log("onHide");
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onUnload() {
+    console.log("onUnload");
   }
 })
